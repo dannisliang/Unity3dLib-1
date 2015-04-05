d8d1b9641dcb6ceb4099a33d9adc38d383f4cd04
zjmmjzzjm <zjmmjzzjm@126.com>
2015-04-05 19:25:14 +0800

    Initial Commit

---
+++ Assets/UnityScripts/MathEx/MathEx.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ZUtil
+{
+	public class MathEx {
+
+		
+		public static Matrix4x4 TranslationMatrix(float x, float y, float z) {
+			Matrix4x4 mat = new Matrix4x4();
+			mat = Matrix4x4.identity;
+			mat.m03 = x;
+			mat.m13 = y;
+			mat.m23 = z;
+			return mat;
+		}
+		
+		public static Matrix4x4 ScalingMatrix(float x, float y, float z) {
+			Matrix4x4 mat = new Matrix4x4();
+			mat = Matrix4x4.identity;
+			mat.m00 = x;
+			mat.m11 = y;
+			mat.m22 = z;
+			return mat;
+		}
+		
+		public static Matrix4x4 AxisRotationMatrix(float ax, float ay, float az, float degreeAngle) {
+			float radianAngle = degreeAngle * ((float)System.Math.PI / 180.0f);
+			Matrix4x4 mat = new Matrix4x4();
+			mat = Matrix4x4.identity;
+			
+			float cos = Mathf.Cos(radianAngle);
+			float sin = Mathf.Sin(radianAngle);
+			
+			mat.m00 = cos + ((ax * ax) * (1.0f - cos));
+			mat.m01 = ((ax * ay) * (1.0f - cos)) - (az * sin);
+			mat.m02 = ((ax * az) * (1.0f - cos)) + (ay * sin);
+			
+			mat.m10 = ((ay * ax) * (1.0f - cos)) + sin;
+			mat.m11 = cos + ((ay * ay) * (1.0f - cos));
+			mat.m12 = ((ay * az) * (1.0f - cos)) - (ax * sin);
+			
+			mat.m20 = ((az * ax) * (1.0f - cos)) - (ay * sin);
+			mat.m21 = ((az * ay) * (1.0f - cos)) + (ax * sin);
+			mat.m22 = cos + ((az * az) * (1.0f - cos));
+			
+			return mat;
+		}
+	}
+
+}
+
---
+++ Assets/UnityScripts/StringEx/StringEx.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using UnityEngine;
+namespace ZUtil
+{
+	public class StringEx {
+
+		
+		
+		public static string EncryptString(string strKey)
+		{
+			
+			byte[] pbyteKey;
+			pbyteKey = ASCIIEncoding.ASCII.GetBytes("MyKey");
+			
+			DESCryptoServiceProvider desCSP = new DESCryptoServiceProvider();
+			desCSP.Mode = CipherMode.ECB;
+			desCSP.Padding = PaddingMode.PKCS7;
+			desCSP.Key = pbyteKey;
+			desCSP.IV = pbyteKey;
+			MemoryStream ms = new MemoryStream();
+			CryptoStream cryptStream = new CryptoStream(ms, desCSP.CreateEncryptor(), CryptoStreamMode.Write);
+			byte[] data = Encoding.UTF8.GetBytes(strKey.ToCharArray());
+			cryptStream.Write(data, 0, data.Length);
+			cryptStream.FlushFinalBlock();
+			
+			string strReturn = Convert.ToBase64String(ms.ToArray());
+			cryptStream = null;
+			ms = null;
+			desCSP = null;
+			return strReturn;
+		}
+		
+		public static string DecryptString(string strKey)
+		{
+			
+			byte[] pbyteKey;
+			pbyteKey = ASCIIEncoding.ASCII.GetBytes("MyKey");
+			
+			DESCryptoServiceProvider desCSP = new DESCryptoServiceProvider();
+			desCSP.Mode = CipherMode.ECB;
+			desCSP.Padding = PaddingMode.PKCS7;
+			desCSP.Key = pbyteKey;
+			desCSP.IV = pbyteKey;
+			MemoryStream ms = new MemoryStream();
+			CryptoStream cryptStream = new CryptoStream(ms, desCSP.CreateDecryptor(), CryptoStreamMode.Write);
+			strKey = strKey.Replace(" ", "+");
+			byte[] data = Convert.FromBase64String(strKey);
+			cryptStream.Write(data, 0, data.Length);
+			cryptStream.FlushFinalBlock();
+			
+			String strReturn = Encoding.UTF8.GetString(ms.GetBuffer());
+			
+			cryptStream = null;
+			ms = null;
+			desCSP = null;
+			
+			return strReturn;
+		}
+		
+		public static bool OtherLanguage(string s)
+		{
+			char[] charArr = s.ToCharArray();
+			
+			foreach (char c in charArr)
+			{            
+				if (char.GetUnicodeCategory(c) == System.Globalization.UnicodeCategory.OtherLetter)
+				{
+					return true;
+				}
+			}
+			
+			return false;
+		}
+
+
+	}
+}
+
